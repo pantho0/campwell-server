7337c523ce1da15205962d52222ddac3246ae1a1
pantho0 <panthomashreky6@gmail.com>
2024-03-18 21:11:00 +0600

    get all camps api added to server

--- index.js
+++ index.js
@@ -21,14 +21,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
+    const campsCollections = client.db('campwell').collection('camps')
+
+    app.get("/api/v1/get-all-camps", async(req,res)=>{
+      const result = await campsCollections.find().toArray();
+      res.send(result)
+    })
+
+
+
+
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
