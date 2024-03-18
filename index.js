30574455b5a7940eda27beefa05e8206af4e8578
pantho0 <panthomashreky6@gmail.com>
2024-03-18 22:05:35 +0600

    camp details api added to server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 //Middlewares
@@ -35,7 +35,12 @@ async function run() {
       res.send(result)
     })
 
-
+    app.get('/api/v1/camp-details/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await campsCollections.findOne(query);
+      res.send(result)
+    })
 
 
     await client.connect();
